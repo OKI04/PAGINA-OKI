ebde7fc75ebafc44f29204087a51e07c86161568
OKI04 <okibasedata@gmail.com>
2025-06-30 15:39:25 -0500

    014

--- back/middlewares/validateToken.js
+++ back/middlewares/validateToken.js
@@ -3,7 +3,12 @@ const { TOKEN_SECRET } = require("../config");
 
 const authRequired = (req, res, next) => {
     console.log("Validing Token");
-    const { token } = req.cookies;
+    let token = req.cookies.token;
+
+    // Si no hay token en cookie, buscar en Authorization header
+    if (!token && req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+        token = req.headers.authorization.split(' ')[1];
+    }
 
     if(!token) return res.status(401).json({ message: "No token, authorization denied"});
 
